743291c1b12a068b273ebdbd9e151b18af7d1519
CarolNiuYan <yanxinyi@grinnell.edu>
2018-06-15 12:11:15 -0500

    delete every thing

--- Empirical/third-party/download_doxygen.sh
+++
@@ -1,8 +0,0 @@
-if [ -d doxygen ]; then                                                         
-        echo "doxygen directory exists!"                                        
-else                                                                            
-        curl -O http://ftp.stack.nl/pub/users/dimitri/doxygen-1.8.11.src.tar.gz
-        tar -xvf doxygen-1.8.11.src.tar.gz  
-        rm doxygen-1.8.11.src.tar.gz                                            
-        mv doxygen-1.8.11 doxygen;                                              
-fi
